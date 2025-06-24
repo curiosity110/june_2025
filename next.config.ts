a71f3648dce3b97d15a90c982b057e8fd60fba2a
curiosity110 <gjorgidelev98@gmail.com>
2025-06-24 12:59:53 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ const nextConfig = {
   images: {
     domains: ["elements-resized.envatousercontent.com", "images.unsplash.com"],
   },
-  experimental: {
-    // @ts-expect-error: allowedDevOrigins is experimental and not yet in type definitions
-    allowedDevOrigins: ['http://192.168.56.1'],
-  },
+  // experimental: {
+  //   // @ts-expect-error: allowedDevOrigins is experimental and not yet in type definitions
+  //   allowedDevOrigins: ['http://192.168.56.1'],
+  // },
 };
 
 export default nextConfig;
